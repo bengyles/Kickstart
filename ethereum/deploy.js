5449419e0df2b577c83b0daf4c5db96f24aff832
bengyles <bengyles@hotmail.com>
2020-08-25 18:26:14 +0200

    installed truffle and dotenv. created deploy script

---
+++ ethereum/deploy.js
@@ -0,0 +1,28 @@
+require('dotenv').config({path: "../.env"});
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC_PHRASE,
+    process.env.INFURA_URL,
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  try {
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({data: '0x' + compiledFactory.bytecode}) // add 0x bytecode
+        .send({from: accounts[0]}); // remove 'gas'
+
+    console.log('Contract deployed to', result.options.address);
+  } catch (err) {
+    console.log(err);
+  }
+};
+deploy();
