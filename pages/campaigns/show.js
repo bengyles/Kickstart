3f32ba3f8d9123b80de7722f02a8dfe4ac7f248f
bengyles <bengyles@hotmail.com>
2020-09-01 01:46:02 +0200

    Show campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,62 @@
 import React, {Component} from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+import {Card} from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
+  renderCards() {
+    const {balance, manager, minimumContribution, requestsCount, approversCount} = this.props;
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of manager',
+        description: 'The manager who created this campaign and can create requests to withdraw money',
+        style: {overflowWrap: 'break-word'},
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum contribution (wei)',
+        description: 'You must contribute at least this amount of wei in order to get in',
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of requests',
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+      },
+      {
+        header: approversCount,
+        meta: 'Number of approvers',
+        description: 'Number of people who have contributed to the campaign',
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign balance (ether)',
+        description: 'Balance is how much money this campaign has left to spend',
+      },
+    ];
+    return <Card.Group items={items}/>;
+  }
+
   render() {
     return (
         <Layout>
           <h3>View Campaign</h3>
+          {this.renderCards()}
         </Layout>);
   }
 }
