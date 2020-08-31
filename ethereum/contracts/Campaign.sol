3405d722dc86284b9855658141a7bca170ad6ed1
bengyles <bengyles@hotmail.com>
2020-08-31 19:41:11 +0200

    updated contract and changed contract address

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.17;
 
-contract CampaignFactory{
+contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum) public{
+    function createCampaign(uint minimum) public {
         address newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
@@ -13,7 +13,7 @@ contract CampaignFactory{
     }
 }
 
-contract Campaign{
+contract Campaign {
     struct Request {
         string description;
         uint value;
@@ -34,7 +34,7 @@ contract Campaign{
         _;
     }
 
-    function Campaign(uint minimum, address creator) public{
+    function Campaign(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
@@ -45,19 +45,19 @@ contract Campaign{
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted{
+    function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-        description: description,
-        value: value,
-        recipient: recipient,
-        complete: false,
-        approvalCount: 0
+        description : description,
+        value : value,
+        recipient : recipient,
+        complete : false,
+        approvalCount : 0
         });
 
         requests.push(newRequest);
     }
 
-    function approveRequest(uint index) public{
+    function approveRequest(uint index) public {
         Request storage request = requests[index];
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
@@ -66,14 +66,28 @@ contract Campaign{
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public restricted{
+    function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
 
-        require(request.approvalCount> (approversCount / 2));
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
 
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary() public view returns (uint, uint, uint, uint, address){
+        return (
+        minimumContribution,
+        this.balance,
+        requests.length,
+        approversCount,
+        manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
 
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import campaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(campaignFactory.interface),
-    '0x045b8a386642091D894da201Deb5E8C69a49099E',
+    '0x8522d9979C343C05B2FEf9770CFeDC666C838226',
 );
 
 export default instance;
