e40b6cfd6affe95f676268f5eea599148bc63637
bengyles <bengyles@hotmail.com>
2020-08-31 18:54:08 +0200

    Create campaign and initialize custom routing

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import factory from '../ethereum/factory';
 import {Card, Button} from 'semantic-ui-react';
 import Layout from '../components/Layout';
+import {Link} from '../routes';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -13,7 +14,7 @@ class CampaignIndex extends Component {
     const items = this.props.campaigns.map(address => {
       return {
         header: address,
-        description: <a>View Campaign</a>,
+        description: <Link route={`campaigns/${address}`}><a>View Campaign</a></Link>,
         fluid: true,
       };
     });
@@ -23,7 +24,12 @@ class CampaignIndex extends Component {
   render() {
     return (
         <Layout>
-          <Button floated="right" content="Create Campaign" icon="add circle" primary/>
+          <Link route="/campaigns/new">
+            <a>
+              <Button floated="right" content="Create Campaign" icon="add circle" primary/>
+            </a>
+          </Link>
+
           <div>{this.renderCampaigns()}</div>
         </Layout>
     );
